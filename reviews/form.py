345bc65d31057f3f3f9c2e2d6cf19d1b2e5ea3c9
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-04-14 01:35:58 -0700

    add validation

--- reviews/form.py
+++ reviews/form.py
@@ -2,4 +2,8 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField()
+    user_name = forms.CharField(
+        label="Your Name",
+        max_length=100,
+        error_messages={"required": "Your name must not be empty"},
+    )
