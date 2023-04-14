355626032bafc3133928c3c68552814e603eebd4
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-04-14 16:34:45 -0700

    use session to mark favorite

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,6 @@
 from django.views.generic.base import TemplateView
+from django.views import View
+from django.http import HttpResponseRedirect
 from django.views.generic import ListView, DetailView, CreateView
 from .form import ReviewForm
 from .models import Review
@@ -31,3 +33,22 @@ class ReviewListView(ListView):
 class ReviewDetailView(DetailView):
     template_name = "reviews/review_detail.html"
     model = Review
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get("favorite_review")
+        print(loaded_review.id, request, favorite_id)
+        context["is_favorite"] = favorite_id == str(loaded_review.id)
+
+        return context
+
+
+class FavoriteView(View):
+    def post(self, request):
+        review_id = request.POST["review_id"]
+        request.session["favorite_review"] = review_id
+        print(request.session["favorite_review"])
+
+        return HttpResponseRedirect("/reviews/" + review_id)
