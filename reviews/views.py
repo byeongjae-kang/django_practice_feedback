16159c6580f7476baa648e410c86176ebd6d3708
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-04-14 03:53:18 -0700

    use CreateView

--- reviews/views.py
+++ reviews/views.py
@@ -1,26 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .form import ReviewForm
 from .models import Review
 
 
 # Create your views here.
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, "reviews/review.html", {"form": form})
-
-    def post(self, request):
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("thank-you")
-
-        return render(request, "reviews/review.html", {"form": form})
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "thank-you"
 
 
 class ThankYouView(TemplateView):
