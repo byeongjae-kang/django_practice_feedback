6acb09d6715b7ccc9ab7e6df5d056d852106ec58
Byeongjae Kang <bart.kang.0719@gmail.com>
2023-04-14 05:44:18 -0700

    complete file upload app

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.CreateProfileView.as_view())]
+urlpatterns = [
+    path("profiles", views.CreateProfileView.as_view()),
+    path("list", views.UserProfileView.as_view()),
+]
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,21 @@
 from django.views import View
+from django.views.generic import CreateView, ListView
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .form import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        return render(request, "profiles/create_profile.html")
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "profiles"
+
+
+class UserProfileView(ListView):
+    template_name = "profiles/user_profile.html"
+    model = UserProfile
+    context_object_name = "profiles"
